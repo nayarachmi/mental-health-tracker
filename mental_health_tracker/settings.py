a8f3dc5078b13222105181a57fcc29ef3a7c0956
nayarachmi <nayaakusuma@gmail.com>
2024-09-11 14:32:04 +0700

    fix pws url

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://naya-kusumahayati-mentalhealthtracker.pbp.cs.ui.ac.id", "https://naya-kusumahayati-mentalhealthtracker.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://naya-kusumahayati-mentalhealthtracker.pbp.cs.ui.ac.id", "https://naya-kusumahayati-mentalhealthtracker.pbp.cs.ui.ac.id"] #fix
